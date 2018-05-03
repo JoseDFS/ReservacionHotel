a192de7c2c2cf233982aa40d5e28fe713b34fb1e
ferna <ferna@DESKTOP-5J6P7HV.mshome.net>
2018-05-02 22:14:01 -0600

    Parte III de ListaCliente

--- src/ListaCliente.java
+++ src/ListaCliente.java
@@ -7,17 +7,36 @@ import java.util.Scanner;
  * @author fernando
  */
 public class ListaCliente {
-   private ArrayList<Cliente> clientes;
-   
-   public ListaCliente (){
-       clientes = new ArrayList<>();
-   }
-   
-   public void add(){
-       Cliente clientes = new Cliente();
-       
-       
-   }
-   
-   
+
+    private ArrayList<Cliente> clientes;
+
+    public ListaCliente() {
+        clientes = new ArrayList<>();
+    }
+
+    Scanner c = new Scanner(System.in);
+
+    public void add() {
+        Cliente cliente = new Cliente();
+
+        System.out.println("Ingrese los nombre del cliente");
+        System.out.print(": ");
+        cliente.setNombre(c.nextLine());
+
+        System.out.println("Ingrese el correo");
+        System.out.print(": ");
+        cliente.setCorreo(c.nextLine());
+
+        System.out.println("Ingrese Telefono");
+        System.out.print(": ");
+        cliente.setTelefono(c.nextLine());
+
+        System.out.println("Ingrese Tarjeta de Credito ");
+        System.out.print(": ");
+        cliente.setTarjetaCredito(c.nextLine());
+
+        clientes.add(cliente);
+
+    }
+
 }
