955d69f3029e30865204f4988eda7c212cdc1ea4
javie <javie@DESKTOP-CHK1H8S>
2018-05-03 13:47:41 -0600

    Parte prinsipal para agregar paquetes

--- src/ListaPaquete.java
+++ src/ListaPaquete.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,28 @@
  * @author javie
  */
 public class ListaPaquete {
+    
+    private ArrayList<Paquete> paquetes;
+
+    public ListaPaquete() {
+        paquetes = new ArrayList<>();
+    }
+
+    Scanner L = new Scanner(System.in);
+
+    public void addListaPaquete() {
+        Paquete paquete = new Paquete();
+        paquetes.add(paquete);
+
+        System.out.println("Escriba el nombre del paquete:");
+        paquete.setNombre(L.nextLine());
+
+        System.out.println("Escriba la cantidad del precio:");
+        paquete.setPrecio(Integer.parseInt(L.nextLine()));
+        
+        System.out.println("Escriba una descripcion para el paquete");
+        paquete.setDescripcion(Integer.parseInt(L.nextLine()))
+
+    }
     
 }
